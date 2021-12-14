ae2813e0a04e587cd0b1a43b9f8aed0ade522e11
Gabriel Lugo <g.alexander.lugo@gmail.com>
2021-12-14 16:31:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# cs361_final1
+Developed with: **VC++2022 x64, C++20**
+###Objects used:
+* a
+* b
+* c
---
+++ src/simpch.hpp
@@ -0,0 +1,29 @@
+//
+// Created by galex on 12/11/2021.
+//
+
+#pragma once
+
+// STD
+#include <cstdlib>
+#include <stdexcept>
+#include <functional>
+#include <algorithm>
+#include <utility>
+#include <memory>
+#include <cmath>
+#include <random>
+
+#include <string>
+#include <array>
+#include <vector>
+#include <queue>
+#include <map>
+
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
+// RepairYard
+#include "util/utilities.hpp"
+
